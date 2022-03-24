9635d642fbab55e4c4b7a7f5f26f88b02d65538d
Sparsh <sparsh0987654321@gmail.com>
2022-03-25 01:07:26 +0530

    added async await in js rather then fetch.then

--- script.js
+++ script.js
@@ -5,22 +5,43 @@
 const jokes = document.querySelector("#joke")
 const jokebtn = document.querySelector("#jokebtn")
 
-const generatejoke = () => {
+const generatejoke = async () => {
 
-    const setHeader = {
-        headers: {
-            Accept: "application/json"
+    //error handling
+    try{
+        const setHeader = {
+            headers: {
+                Accept: "application/json"
+            }
         }
+    
+        const response = await fetch('https://icanhazdadjoke.com/', setHeader)
+        const data = await response.json()
+        jokes.innerHTML = data.joke 
+    }catch(err){
+        console.log(`the error is ${err}`);
     }
-
-    fetch('https://icanhazdadjoke.com/', setHeader)
-    .then((res) => res.json() )   
-    .then((data) =>{
-        jokes.innerHTML = data.joke
-    }).catch((error) =>{
-        console.log(error)
-    })
 }
+
+
+// without async and await
+
+// const generatejoke = () => {
+
+//     const setHeader = {
+//         headers: {
+//             Accept: "application/json"
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com/', setHeader)
+//     .then((res) => res.json() )   
+//     .then((data) =>{
+//         jokes.innerHTML = data.joke
+//     }).catch((error) =>{
+//         console.log(error)
+//     })
+// }
 
 jokebtn.addEventListener("click", generatejoke)
 generatejoke()
